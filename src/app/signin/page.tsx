3422d65e24ab5f5633acb1b3e4b522ca789e480e
arrancy <ruturajchondekar@gmail.com>
2025-11-09 02:06:55 +0530

    added signout button to test signout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function Signin() {
+  const [email, setEmail] = useState<string>("");
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <button
-        onClick={() => signIn("google")}
-        className="border-2 rounded-md p-2 text-white text-2xl"
-      >
-        signin
-      </button>
+      <div className="px-4 py-4 border-2 border-white">
+        <label>signin with email:</label>
+        <input
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(event.target.value)
+          }
+          type="text"
+          className="w-full border-2 border-white outline-none focus:ring-1 ring-slate-700 ring-0"
+        />
+        <button
+          onClick={() => signIn("email", { email })}
+          className="border-2 rounded-md p-2 text-white text-2x"
+        >
+          signin with email
+        </button>
+        <button
+          onClick={() => signIn("google")}
+          className="border-2 rounded-md p-2 text-white text-2xl"
+        >
+          signin
+        </button>
+      </div>
     </div>
   );
 }
