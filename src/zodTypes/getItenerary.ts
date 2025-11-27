4117b6578daf210319b11852e26318fbec54f822
arrancy <ruturajchondekar@gmail.com>
2025-11-27 23:57:16 +0530

    more notes

--- src/zodTypes/getItenerary.ts
+++ src/zodTypes/getItenerary.ts
@@ -2,11 +2,11 @@ import z from "zod";
 export const getItenerarySchema = z.object({
   fromPlace: z.object({
     name: z.string().min(1),
-    class: z.enum(["place"]),
+    osm_key: z.enum(["place"]),
   }),
   toPlace: z.object({
     name: z.string().min(1),
-    class: z.enum(["place", "historic", "tourism"]),
+    osm_key: z.enum(["place", "historic", "tourism"]),
   }),
   startDate: z.iso.date(),
   endDate: z.iso.date(),
@@ -15,3 +15,7 @@ export const getItenerarySchema = z.object({
 // so we can take the display name for sure , on top of that we can also take class, class can be place, tourism , or historic that's it, and we can add in the system prompt that
 // if the class is historic or tourism then we can suggest the LLM in the prompt that you can add keep visiting that place at the top priority
 // and if the duration allows then include other things nearby that place in the itinerary
+// now that i have discovered even the most legit display_names return multiple results, i am sure  we need to query by placeid after getting the response
+// on top of all this we need to learn rate limiting so that we don't end up being DDOSed and increase our bills by calling that location
+// api many times
+// locationiq not happening, photon very similar, search by id not happening, max we can do is search , basically the same ,
