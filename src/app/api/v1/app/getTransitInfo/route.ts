3e2c29938261c848bcb3af98fc4ddc6f1352a432
arrancy <ruturajchondekar@gmail.com>
2025-12-05 22:17:13 +0530

    added structured output endpoint for indian railways journey

--- src/app/api/v1/app/getTransitInfo/route.ts
+++ src/app/api/v1/app/getTransitInfo/route.ts
@@ -1,12 +1,16 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
-import { getTransitInfoSchema } from "@/zodTypes/getTransitInfo";
+import {
+  getTransitInfoSchema,
+  transitInfoLlmSchema,
+} from "@/zodTypes/getTransitInfo";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 import Groq from "groq-sdk";
 import { getTransitInfoSystemPrompt } from "@/lib/prompts/getTransitInfoSystemPrompt";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
   if (!session || !session.user?.id) {
     return NextResponse.json({ msg: "unauthenticared" }, { status: 401 });
@@ -21,6 +25,20 @@ export async function GET(req: NextRequest) {
       { role: "system", content: getTransitInfoSystemPrompt },
       { role: "user", content: JSON.stringify(reqBody) },
     ],
-    model: "moonshotai/kimi-k2-instruct",
+    response_format: {
+      type: "json_schema",
+      json_schema: {
+        name: "transit-info-llm-schema",
+
+        schema: z.toJSONSchema(transitInfoLlmSchema),
+      },
+    },
+    model: "openai/gpt-oss-20b",
   });
+
+  const rawResult = JSON.parse(response.choices[0].message.content || "{}");
+  const validatedResult = transitInfoLlmSchema.safeParse(rawResult);
+  return !validatedResult.success
+    ? NextResponse.json({ msg: "internal server error" }, { status: 500 })
+    : NextResponse.json({ result: rawResult });
 }
--- src/zodTypes/getTransitInfo.ts
+++ src/zodTypes/getTransitInfo.ts
@@ -4,3 +4,14 @@ export const getTransitInfoSchema = z.object({
   getItineraryInput: getItenerarySchema,
   itinerary: z.string().min(1),
 });
+
+export const transitInfoLlmSchema = z.object({
+  domesticTrip: z.boolean(),
+  internationalTrip: z.boolean(),
+  usesIndianRailways: z.object({
+    applicable: z.boolean(),
+    journeys: z.array(
+      z.object({ source: z.string().min(1), destination: z.string().min(1) })
+    ),
+  }),
+});
